3274b5c9e6a49a588a84f7750e933fb8d0c9d1d8
gopallchoudhary <gopalchoudhary521@gmail.com>
2026-05-03 12:01:14 +0530

    add counter

--- react-refresher/src/App.jsx
+++ react-refresher/src/App.jsx
@@ -1,7 +1,18 @@
+import { useState } from "react";
 function App() {
+	const [count, setCount] = useState(0);
+	const increment = () => {
+		setCount((count) => count + 1);
+	};
+	const decrement = () => {
+		setCount((count) => Math.max(count - 1, 0));
+	};
+
 	return (
 		<div>
-			<h1>App</h1>
+			<h1>Counter: {count}</h1>
+			<button onClick={increment}>➕</button>
+			<button onClick={decrement}>➖</button>
 		</div>
 	);
 }
--- react-refresher/src/main.jsx
+++ react-refresher/src/main.jsx
@@ -4,9 +4,6 @@ import H1 from "./H1";
 const name = "Gopal";
 createRoot(document.getElementById("root")).render(
 	<>
-		<H1 title="web dev" />
-		<H1 title="app dev" />
-		<H1 title="louda dev"/>
 		<App />
 	</>,
 );
